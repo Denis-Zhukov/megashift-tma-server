953db66ca477c4b0922e77533d2ffbdabc77a190
Жуков Денис Владимирович <DZhukov4@alfabank.ru>
2026-02-20 02:14:59 +0300

    feat: add user module to statistics

--- src/statistics/statistics.module.ts
+++ src/statistics/statistics.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { StatisticsService } from './statistics.service';
 import { StatisticsController } from './statistics.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { UserModule } from '../user/user.module';
 
 @Module({
   controllers: [StatisticsController],
   providers: [StatisticsService, PrismaService],
+  imports: [UserModule],
 })
 export class StatisticsModule {}
