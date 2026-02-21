60fa284bfa64409da5c4de2fc9ee59533d9429c7
Denis Zhukov <dzhukov@leantech.ai>
2026-02-22 02:03:21 +0300

    feat: helmet, rateLimit and bodyParser is added

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,29 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TmaGuard } from './utils/guards/tma.guard';
 import { WinstonLogger } from './logger/winston-logger.service';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const server = app.getHttpAdapter().getInstance();
+  server.set('trust proxy', 1);
+
+  app.use(helmet());
+
+  app.use(bodyParser.json({ limit: '1mb' }));
+  app.use(bodyParser.urlencoded({ extended: true, limit: '100kb' }));
+
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000,
+      limit: 100,
+      message: 'Слишком много запросов с этого IP, попробуйте позже',
+      standardHeaders: true,
+      legacyHeaders: false,
+    }),
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -19,6 +39,17 @@ async function bootstrap() {
   const logger = new WinstonLogger();
   app.useLogger(logger);
 
+  // app.enableCors({
+  //   origin: ['https://yourdomain.com'],
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true,
+  // });
+
   await app.listen(8000);
+  logger.log('Application is running on port 8000');
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error('Bootstrap failed', err);
+  process.exit(1);
+});
